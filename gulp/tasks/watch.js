59398be08a37e7557348cfa6ed04aa229274b69b
Karen <karen81e@gmail.com>
2018-09-07 18:29:05 +0200

    completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+	
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+	
+	gulp.watch('./app/index.html', function() {
+		browserSync.reload();
+	});
+	
+	gulp.watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('cssInject');
+	});
+});
+
+gulp.task('cssInject', ['styles'],function() {
+	return gulp.src('./app/assets/styles/styles.css').pipe(browserSync.stream());
+});
